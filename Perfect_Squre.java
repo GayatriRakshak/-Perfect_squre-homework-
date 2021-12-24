184e1b3e103de7f6498ab6ec61ea135568e8104b
MICKEY935974 <69594578+MICKEY935974@users.noreply.github.com>
2021-12-24 12:10:04 +0530

    Add files via upload

---
+++ Perfect_Squre.java
@@ -0,0 +1,19 @@
+
+public class Perfect_Squre {
+
+	public static void main(String[] args) {
+		 int i;
+		 int num=10;
+		 for(i=1;i<num;i++)
+		 {
+			 if(i*i==num)
+			 {
+				 System.out.println("Given number is squre of "+i);
+				 return;
+			 }
+			 
+		 }
+
+	}
+
+}

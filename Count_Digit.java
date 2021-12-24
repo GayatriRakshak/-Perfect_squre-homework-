8a5201ead71957558b9b9d646d8266f5833bd180
MICKEY935974 <69594578+MICKEY935974@users.noreply.github.com>
2021-12-24 12:11:22 +0530

    Add files via upload

---
+++ Count_Digit.java
@@ -0,0 +1,15 @@
+
+public class Count_Digit {
+
+	public static void main(String[] args) {
+		int num= 157543279;
+		int count=0;
+		while(num!=0) 
+		{
+			num=num/10;
+			count=count+1;
+		}
+		System.out.println(count);
+	}
+
+}
